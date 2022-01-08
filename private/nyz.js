a38f5a79e93177ea40680ccb2b01db742d60da2d
Saipul Anuar <drakipul1016@gmail.com>
2022-01-08 20:17:37 +0700

    first commit

---
+++ private/nyz.js
@@ -0,0 +1,288 @@
+exports.paymentstore = () => {
+return `*PAYMENT STORE*
+Dana : 0882-7926-8363
+Ovo : VIA QR
+Pulsa : VIA QR AJA
+
+Note :
+Jika Sudah Transfer Wajib Mengirimkan
+Bukti Transfer Ke Owner Bot 
+Agar Bisa Cepat Di Proses
+`
+}
+
+exports.diamondFF = () => {
+return`*DROP DATA DIAMOND FF*
+• VIA ID
+• LEGAL 100%
+• PROSES 1-3 MENIT
+
+*50 💎 Rp 6.672*
+*70 💎 Rp 9.174*
+*100 💎 Rp 13.344*
+*140 💎 Rp 18.348*
+*210 💎 Rp 27.522*
+*355 💎 Rp 45.870*
+*500 💎 Rp 65.052*
+*720 💎 Rp 91.740*
+*1000 💎 Rp 130.104*
+*2000 💎 Rp 250.200*
+*4000 💎 Rp 500.400*
+
+*Member Mingguan Rp 27.800*
+*Member Bulanan Rp 139.000*
+
+Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+`
+}
+
+exports.hagostore = () => {
+return `*DROP DATA DIAMOND HAGO*
+• VIA ID
+• LEGAL 100%
+• PROSES 1-3 MENIT
+
+*6 💎 Rp 1.900*
+*45 💎 Rp 14.800*
+*90 💎 Rp 29.000*
+*225 💎 Rp 73.000*
+*375 💎 Rp 120.000*
+*900 💎 Rp 290.000*
+*1650 💎 Rp 530.000*
+
+Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+`
+}
+
+exports.chipstore = () => {
+return `Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID & Server Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+
+*DROP DATA CHIP*
+• VIA ID + NICK
+• LEGAL 100%
+• PROSES 1-5 MENIT
+• STOK UNLIMITED
+
+*10M Koin Emas-D Rp 2.500*
+*15M Koin Emas-D Rp 3.000*
+*30M Koin Emas-D Rp 5.000*
+*60M Koin Emas-D Rp 8.000*
+*100M Koin Emas-D Rp 13.000*
+*200M Koin Emas-D Rp 20.000*
+*400M Koin Emas-D Rp 39.000*
+*600M Koin Emas-D Rp 56.000*
+*1B Koin Emas-D Rp 69.000*
+*2B Koin Emas-D Rp 138.000*
+*3B Koin Emas-D Rp 207.000*
+*4B Koin Emas-D Rp 276.000*
+*5B Koin Emas-D Rp 345.000*
+*6B Koin Emas-D Rp 414.000*
+*7B Koin Emas-D Rp 483.000*
+
+Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+`
+}
+
+exports.diamondML = () => {
+return `*DROP DATA DIAMOND ML*
+• VIA ID & SERVER
+• LEGAL 100%
+• PROSES 1-5 MENIT
+
+*86 💎 Rp 18.748*
+*172 💎 Rp 37.230*
+*257 💎 Rp 56.283*
+*344 💎 Rp 75.336*
+*429 💎 Rp 93.951*
+*514 💎 Rp 112.566*
+*600 💎 Rp 131.400*
+*706 💎 Rp 154.614*
+*878 💎 Rp 192.282*
+*963 💎 Rp 210.897*
+*1412 💎 Rp 300.756*
+*2195 💎 Rp 452.170*
+*3688 💎 Rp 748.664*
+*4394 💎 Rp 891.982*
+*5532 💎 Rp 1.122.996*
+*9288 💎 Rp 1.885.464*
+
+*Starlight Membership Bonus 12 💎 Rp 95.000*
+*Twilight Pass [PROMO ]  Rp 95.000*
+*Starlight Membership  Rp 127.000*
+*Twilight  Rp 127.000*
+*Starlight Membership Plus [PROMO ] Rp 240.000*
+*Mobile Legend Member Plus Rp 285.000*
+`
+}
+
+exports.garenashell = () => {
+return `*DROP DATA GARENA SHELL*
+• VIA ID
+• LEGAL 100%
+• PROSES 1-5 MENIT
+• STOK UNLIMITED
+
+*33 SHELL 💰	Rp 10.000*
+*66 SHELL 💰	Rp 20.000*
+*99 SHELL 💰	Rp 30.000*
+*165 SHELL 💰	Rp 50.000*
+*330 SHELL 💰	Rp 100.000*
+
+Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+`
+}
+
+exports.cbcashstore = () => {
+return `*DROP DATA POINT BLANK*
+• VIA ID
+• LEGAL 100%
+• PROSES 1-5 MENIT
+• STOK UNLIMITED
+
+*1200 PB CASH 💸	Rp 11.000*
+*2400 PB CASH 💸	Rp 22.000*
+*6000 PB CASH 💸	Rp 55.000*
+*12000 PB CASH 💸	Rp 110.000*
+*24000 PB CASH 💸	Rp 220.000*
+*36000 PB CASH 💸	Rp 330.000*
+*60000 PB CASH 💸	Rp 550.000*
+Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+`
+}
+
+exports.codmstore = () => {
+return `*DROP DATA CALL OF DUTY*
+• VIA ID
+• LEGAL 100%
+• PROSES 1-5 MENIT
+• STOK UNLIMITED
+
+*31 CP 💸	Rp 5.000*
+*62 CP 💸	Rp 10.000*
+*127 CP 💸	Rp 20.000*
+*317 CP 💸  Rp 50.000*
+*634 CP 💸  Rp 100.000*
+*1373 CP 💸 Rp 200.000*
+*2059 CP 💸 Rp 300.000*
+*3564 CP 💸 Rp 500.000*
+
+Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+`
+}
+
+exports.pubgstore = () => {
+return `*DROP DATA UC PUBG*
+• VIA ID
+• LEGAL 100%
+• PROSES 1-5 MENIT
+• STOK UNLIMITED
+
+*50 💸	Rp 9.390*
+*100 💸	Rp 18.565*
+*125 💸	Rp 22.740*
+*150 💸	Rp 28.205*
+*250 💸	Rp 45.950*
+*500 💸	Rp 91.505*
+*600 💸	Rp 100.330*
+*700 💸	Rp 131.430*
+*800 💸	Rp 149.680*
+*1250 💸 Rp 230.230*
+*1350 💸 Rp 260.180*
+*1500 💸 Rp 275.205*
+*2500 💸 Rp 455.205*
+*3000 💸 Rp 540.680*
+*3500 💸 Rp 625.180*
+*4000 💸 Rp 719.180*
+*5000 💸 Rp 920.180*
+*7000 💸 Rp 1.329.980*
+
+Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+`
+}
+
+exports.donasi = () => {
+return `*HAI KAK MAU BENERAN DONASI ATAU TIDAK KAK*
+*SEMOGA TIDAK DILIHAT DOANG*
+
+*PAYMENT BOT*
+DANA: (0882-7926-8363)
+OVO: VIA QR
+PULSA: VIA QR AJA
+GOPAY: VIA QR
+QRIS: SCAN DI ATAS
+
+*TERIMA KASIH KAK*
+*LOVE YOU😋*`
+}
+
+exports.info1 = () => { 
+    return`🐳 = $200
+🦈 = $121
+🐬 = $104
+🐋 = $94
+🐟 = $87
+🐠 = $79
+🦐 = $62
+🦑 = $34
+🦀 = $17
+🐚 = $2
+*NOTE* : TETAPLAH BERBURU KAWAN. WALAUPUN TIDAK BERGUNA SEPERTI ANDA`
+  }
+exports.info2 = () => { 
+    return`🐔 = $200
+🦃 = $121
+🐿 = $104
+🐐 = $94
+🐏 = $87
+🐖 = $79
+🐑 = $62
+🐎 = $34
+🐺 = $17
+🦩 = $2
+*NOTE* : TETAPLAH BERBURU KAWAN. WALAUPUN TIDAK BERGUNA SEPERTI ANDA`
+}
+exports.info3 = () => { 
+    return`🦋 = $200
+🕷 = $121
+🐝 = $104
+🐉 = $94
+🦆 = $87
+🦅 = $79
+🕊 = $62
+🐧 = $34
+🐦 = $17
+🦇 = $2
+*NOTE* : TETAPLAH BERBURU KAWAN. WALAUPUN TIDAK BERGUNA SEPERTI ANDA`
+}
